d81550a3a9bd985f5d7145c61f133eb5b5ed282d
Tushar Mathur <tusharmath@gmail.com>
2019-12-30 07:24:32 +0530

    style(prettier): format code

--- src/WorkerId.ts
+++ src/WorkerId.ts
@@ -1,6 +1,8 @@
 import {QIO} from '@qio/core'
+
 import {WORKER_ID_ENV} from './MasterProgram'
 export const workerId = QIO.lift(() => {
   const env = Number(process.env[WORKER_ID_ENV])
+
   return Number.isFinite(env) ? Number(env) : 999
 })
